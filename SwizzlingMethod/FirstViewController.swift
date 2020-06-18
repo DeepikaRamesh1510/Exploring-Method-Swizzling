342087354b72b179ae010f921ab6c86bddb30688
Deepika <deepika.ram@anywhere.co>
2020-06-18 19:30:13 +0530

    Implementation of swizzling method

---
+++ SwizzlingMethod/FirstViewController.swift
@@ -0,0 +1,32 @@
+//
+//  FirstViewController.swift
+//  SwizzlingMethod
+//
+//  Created by Deepika on 18/06/20.
+//  Copyright © 2020 Deepika. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+//    override func viewWillAppear(_ animated: Bool) {
+//        
+//    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- SwizzlingMethod/UIKit Extension.swift
+++ SwizzlingMethod/UIKit Extension.swift
@@ -7,3 +7,25 @@
 //
 
 import Foundation
+import UIKit
+
+extension  UIViewController {
+    
+    @objc dynamic func swizzle_viewWillAppear(_ animated: Bool) {
+        swizzle_viewWillAppear(animated)
+        print("The view is being appeared!")
+    }
+    
+    static func exchangeViewWillAppear() {
+        
+        let oldMethod = class_getInstanceMethod(UIViewController.self, #selector(UIViewController.viewWillAppear(_:)))
+        let newMethod = class_getInstanceMethod(UIViewController.self, #selector(UIViewController.swizzle_viewWillAppear(_:)))
+        
+        guard let defaultMethod = oldMethod, let swizzlingMethod = newMethod else {
+            assertionFailure("The selector doesn't belong to type specified!")
+            return
+        }
+        
+        method_exchangeImplementations(defaultMethod, swizzlingMethod)
+    }
+}
