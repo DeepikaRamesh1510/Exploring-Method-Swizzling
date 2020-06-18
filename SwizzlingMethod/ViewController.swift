6677e5d9b9a3ba2821cfbaee4dcc71af59f89a58
Deepika <deepika.ram@anywhere.co>
2020-06-18 18:22:41 +0530

    Initial Commit

---
+++ SwizzlingMethod/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwizzlingMethod
+//
+//  Created by Deepika on 18/06/20.
+//  Copyright © 2020 Deepika. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
